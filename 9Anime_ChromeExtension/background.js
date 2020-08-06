0d600d91ad7df341d4272e478d8084c1a404ba1c
FLin <>
2020-08-05 19:38:04 -0500

    Changed background.js and popup.js

--- 9Anime_ChromeExtension/background.js
+++ 9Anime_ChromeExtension/background.js
@@ -1,13 +1,20 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({ color: '#3aa757' }, function() {
-        console.log("The color is green.");
-    });
+
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'developer.chrome.com' },
+                pageUrl: { hostEquals: '9anime.ru' },
+                css: ["span.tab.active[data-name ='35']"]
+            }), new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { hostEquals: '9anime.ru' },
+                css: ["span.tab[data-name='35']"]
             })],
             actions: [new chrome.declarativeContent.ShowPageAction()]
+
+
+
+
+
         }]);
     });
 });
--- 9Anime_ChromeExtension/popup.js
+++ 9Anime_ChromeExtension/popup.js
@@ -1,6 +1,8 @@
+location = location
+
 let changeColor = document.getElementById('changeColor');
 
 chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+
+
 });

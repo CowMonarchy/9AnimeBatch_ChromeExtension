fd894c6084121900ce0c5fda9e0e37a9d83d7d69
FLin <>
2020-08-06 00:32:46 -0500

    updated  popup UI

--- 9Anime_ChromeExtension/background.js
+++ 9Anime_ChromeExtension/background.js
@@ -5,14 +5,8 @@ chrome.runtime.onInstalled.addListener(function() {
             conditions: [new chrome.declarativeContent.PageStateMatcher({
                 pageUrl: { hostEquals: '9anime.ru' },
                 css: ["span.tab.active[data-name ='35']"]
-            }), new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: '9anime.ru' },
-                css: ["span.tab[data-name='35']"]
             })],
             actions: [new chrome.declarativeContent.ShowPageAction()]
-
-
-
 
 
         }]);
--- 9Anime_ChromeExtension/popup.js
+++ 9Anime_ChromeExtension/popup.js
@@ -1,5 +1,3 @@
-location = location
-
 let changeColor = document.getElementById('changeColor');
 
 chrome.storage.sync.get('color', function(data) {

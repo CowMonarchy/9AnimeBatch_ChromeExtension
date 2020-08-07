5eb8d896775b6034ed5130cd85afb6183d0b00ab
FLin <>
2020-08-07 14:28:01 -0500

    place code under a function in popup.js

--- 9Anime_ChromeExtension/popup.js
+++ 9Anime_ChromeExtension/popup.js
@@ -1,2 +1,7 @@
-var animeName = document.getElementById("#animeName");
-animeName.innerHTML = "Yeet";
+function getAnimeName() {
+    //Pulls anime name from anime page h2 element and pastes text it into popup.html h2
+    var animeName = document.querySelector("h2.title[data-jtitle]");
+    var setName = document.getElementById("animeName");
+    setName.innerHTML = document.URL;
+    return setName;
+}

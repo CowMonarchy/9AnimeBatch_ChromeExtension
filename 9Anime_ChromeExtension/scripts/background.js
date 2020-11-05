de3e410b46180de774b8f7bb38433f51895893bd
FLin <>
2020-11-04 20:32:30 -0600

    - Updated Html UI - Organized Folder Struct - Added Settings Html

--- 9Anime_ChromeExtension/scripts/background.js
+++ 9Anime_ChromeExtension/scripts/background.js
@@ -1,10 +1,10 @@
 chrome.runtime.onInstalled.addListener(function() {
-
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: '9anime.ru' },
-                css: ["span.tab.active[data-name ='35']"]
+                pageUrl: { hostEquals: 'www12.9anime.ru', urlContains: '/watch' }
+
+
             })],
             actions: [new chrome.declarativeContent.ShowPageAction()]
 

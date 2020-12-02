2c7fe1c783b8af06cb39b49a0d725ffa1e9af98f
FLin <>
2020-12-01 19:57:51 -0600

    Made A lot of changes, I can type more on my commits so here is a full rundown : - Created HTML for popup page - Created Inject script (copyDOM.js) - Inject script gets anime title and highest episode number - Inject scrip sends that info to popup.js -popup.js updates popup.html based on this new information
    Some refactoring and reorganization is needed.

--- GoGoDownloader_ChromeExtension/scripts/copyDOM.js
+++ GoGoDownloader_ChromeExtension/scripts/copyDOM.js
@@ -1,17 +1,27 @@
 //
 //
 //
+//
+console.log("Script Injected");
 chrome.runtime.onMessage.addListener(
     function(request, sender, response) {
         if (request.message === "Get_Anime_Info") {
             //Obtaining Info From Webpage
             var link = $("link[rel = 'canonical']").eq(0).attr('href');
             var showName = $(".anime-info > a").text();
-            //var highestEpisode
+
+            //Grabbing Episode lists and sorting to place inside of highest episode
+            var episodeList = $("a[ep_end]");
+            var sortedList = [];
+            for (var i = episodeList.length - 1; i >= 0; i--)
+                sortedList.push($(episodeList[i]).attr("ep_end"));
+
+
+            var highestEpisode = sortedList[0];
 
 
             //Returning that Info to popup.js
-            response({ animeName: showName });
+            response({ animeName: showName, maxNumber: highestEpisode });
         }
     }
 );
--- GoGoDownloader_ChromeExtension/scripts/popup.js
+++ GoGoDownloader_ChromeExtension/scripts/popup.js
@@ -2,26 +2,13 @@
 //
 //
 //
-//Page Objects
-var animeName = "Default Name";
-var highestEpisode = 1;
-//Page Objects 
-
-
-
 //Sends Anime Info Request to copyDom.js / Recieves Info and updates page objects (content script)
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { message: "Get_Anime_Info" }, function(response) {
 
-        animeName = response.animeName;
+        document.getElementById('displayAnimeName').innerText = response.animeName;
+        document.getElementById('StartEpisodeIn').setAttribute('max', response.maxNumber);
+        document.getElementById('EndingEpisodeIn').setAttribute('max', response.maxNumber)
     });
 });
 //-------------------------------------------------------------------------------------------------
-
-
-
-//Updating Extension HTML with Anime Info
-document.getElementById('displayAnimeName').innerHTML = animeName;
-document.getElementById('StartEpisodeIn');
-document.getElementById('EndingEpisodeIn');
-//---------------------------------------

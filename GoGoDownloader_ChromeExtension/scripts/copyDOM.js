cd54105aa492462ea8dcadf3db94742071e64a7b
FLin <>
2020-12-01 18:43:18 -0600

    Message received, updating extention element chang

--- GoGoDownloader_ChromeExtension/scripts/copyDOM.js
+++ GoGoDownloader_ChromeExtension/scripts/copyDOM.js
@@ -2,19 +2,16 @@
 //
 //
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    function(request, sender, response) {
         if (request.message === "Get_Anime_Info") {
             //Obtaining Info From Webpage
-
-            console.log("Step 02 : Message Received")
             var link = $("link[rel = 'canonical']").eq(0).attr('href');
-            console.log("Step 03 : Info Grabbed");
-
+            var showName = $(".anime-info > a").text();
+            //var highestEpisode
 
 
             //Returning that Info to popup.js
-            sendResponse({ farewell: "Return_Anime_Info" });
-            console.log("Step 04 : Info Sent");
+            response({ animeName: showName });
         }
     }
 );
--- GoGoDownloader_ChromeExtension/scripts/popup.js
+++ GoGoDownloader_ChromeExtension/scripts/popup.js
@@ -1,25 +1,27 @@
 //
 //
-// 
+//
+//
 //Page Objects
-var animeName = "";
+var animeName = "Default Name";
+var highestEpisode = 1;
 //Page Objects 
 
 
-//Sends Anime Info Request to copyDom.js (content script)---------------------------------
+
+//Sends Anime Info Request to copyDom.js / Recieves Info and updates page objects (content script)
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { message: "Get_Anime_Info" }, function(response) {
-        console.log("Step 01 : Message Sent");
+
+        animeName = response.animeName;
     });
 });
+//-------------------------------------------------------------------------------------------------
 
 
-//Retriving Anime Info and Assigning it to Respected Variables
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.farewell === "Return_Anime_Info") {
-            console.log("Step 05 : Info Recieved");
-        }
-    }
-);
-//-----------------------------------------------------------------------------------------
+
+//Updating Extension HTML with Anime Info
+document.getElementById('displayAnimeName').innerHTML = animeName;
+document.getElementById('StartEpisodeIn');
+document.getElementById('EndingEpisodeIn');
+//---------------------------------------
